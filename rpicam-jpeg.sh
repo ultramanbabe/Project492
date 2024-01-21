817acaa6a546ae22f2c747a5eef761462f677739
Puriwong <ultraman_babeclub@hotmail.com>
2024-01-22 00:25:59 +0700

    moved from master to main

--- rpicam-jpeg.sh
+++ rpicam-jpeg.sh
@@ -2,4 +2,6 @@
 
 #rpicam-jpeg --width 2592 --height 1944 -q 100 -o test.jpg
 
-rpicam-jpeg --width 2592 --height 1944 -q 100 -t 5000 --shutter 200000 --gain 2 -n  -o test.jpg
+rpicam-jpeg --width 2592 --height 1944 -q 100 --shutter 300000 --gain 1.5 -n  -o test.jpg
+
+
