810351327c816b75aafdf65912ef006477ba7f77
Puriwong <ultraman_babeclub@hotmail.com>
2024-01-22 19:39:27 +0700

    before go drunk today

--- rpicam-jpeg.sh
+++ rpicam-jpeg.sh
@@ -2,6 +2,7 @@
 
 #rpicam-jpeg --width 2592 --height 1944 -q 100 -o test.jpg
 
-rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 300000 --gain 1.5 -n  -o test.jpg
+#rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 300000 --gain 1.5 -n  -o test.jpg
 
+rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 200000 --gain 1.5 -n -o ./images/test.jpg
 
