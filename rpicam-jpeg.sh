2791819481bc56ae92bcd92b56a09f7f469e22c4
Puriwong <ultraman_babeclub@hotmail.com>
2024-02-01 13:59:25 +0700

    captured image with time to filename

--- rpicam-jpeg.sh
+++ rpicam-jpeg.sh
@@ -4,5 +4,5 @@
 
 #rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 300000 --gain 1.5 -n  -o test.jpg
 
-rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 200000 --gain 1.5 -n -o ./images/test.jpg
+rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 200000 --gain 1.5 -n -o ./images/"$(date +%F-%T)".jpg
 
