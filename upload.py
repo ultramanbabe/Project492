48b07478142d57659a1b4da98ba1a1e45aac1da0
Puriwong <ultraman_babeclub@hotmail.com>
2024-02-05 23:42:53 +0700

    edited upload.py

--- upload.py
+++ upload.py
@@ -1,50 +1,92 @@
-#!/usr/bin/python3
+import os
+import time
+import sys
+from google.cloud import  storage
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-import os 
-from google.cloud import storage
-from firebase_admin import credentials, initialize_app
+# Set the name of my Google Cloud Storage Bucket
+bucket_name = 'project492-9a253.appspot.com'
 
-#service_account_key_path = '/home/pi/project492/ServiceAccountKey.json'
-service_account_key_path = '/home/pi/project492/ServiceAccountKey_appEngineDefault.json' 
-local_images_folder = '/home/pi/project492/images/'
-remote_images_folder = 'images/' 
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/pi/project492/ServiceAccountKey.json'
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/pi/project492/ServiceAccountKey_appEngineDefault.json'
-# Initialize Firebase Admin SDK
+# Set Service Account Key
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/home/pi/project492/ServiceAccountKey.json'
 
-cred = credentials.Certificate(service_account_key_path)
-initialize_app(cred, {'storageBucket': 'project492-9a253.appspot.com'})
-
-# Initialize Google Cloud Storage client
+# Initialize Google Cloud Storage Client
 storage_client = storage.Client()
 
-# Get the bucket reference
-bucket = storage_client.get_bucket('project492-9a253.appspot.com')
+# Global variable for timestamp
+timestamp = ''
 
-def upload_images(local_folder, remote_folder):
-    for root, dirs, files in os.walk(local_folder):
+# Function to update the timestamp
+def update_timestamp():
+    global timestamp
+    timestamp = datetime.now().strftime('%d-%m-%Y-%T')
+
+# Watchdog event handler for monitoring file system changes
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.is_directory:
+            return
+        elif event.event_type == 'created' or event.event_type == 'modified':
+            log_message = f"{timestamp} - Change detected: {event.src_path}"
+            upload_image(event.src_path, "images")
+            log_to_file(log_message, 'upload_log.log')
+
+# Function to upload an image to Google Cloud Storage
+def upload_image(local_path, cloud_path):
+    blob_name = cloud_path + "/" + os.path.relpath(local_path, start='/home/pi/project492/images')
+
+    # Upload image to Google Cloud Storage
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(local_path)
+
+    log_message = f"{timestamp} - Image uploaded Succesfully. Bucket: {bucket_name}, Blob: {blob_name}"
+    log_to_file(log_message, 'upload_log.log')
+
+# Function to perform initial scan and upload existing image
+def retroactively_upload(local_path, cloud_path):
+    for root, dirs, files in os.walk(local_path):
         for file in files:
-            local_file_path = os.path.join(root, file)
-            remote_file_path = os.path.join(remote_folder, os.path.relpath(local_file_path, local_folder))
+            file_path = os.path.join(root, file)
+            upload_image(file_path, cloud_path)
 
-            # Check if the image is already exists in Firebase Cloud Storage
-            if not check_image_exists(remote_file_path):
-                blob = bucket.blob(remote_file_path)
-                blob.upload_from_filename(local_file_path)
-                print(f"Uploaded: {local_file_path} to {remote_file_path}")
-            else:
-                print(f"Skipped: {local_file_path} (Image already exists)")
+# Function to log messages to a log file
+def log_to_file(message, filename):
+    with open(filename, 'a') as log_file:
+        log_file.write(f'{message}\n')
 
-# Function to check if image already exists in Firebase Cloud Storage
-def check_image_exists(remote_file_path):
+# Function to start monitoring the images folder
+def start_monitoring():
+    event_handler = MyHandler()
+    observer = Observer()
+
+    # Set the local path on my Raspberry Pi
+    local_path = '/home/pi/project492/images'
+
+    # Perform initial scan and upload existing images
+    retroactively_upload(local_path, 'images')
+
+    observer.schedule(event_handler, path=local_path, recursive=True)
+    observer.start()
+
     try:
-        blob = bucket.blob(remote_file_path)
-        blob.reload()
-        return True
-    except storage.exceptions.NotFound:
-        return False
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        opserver.stop()
 
-# Start the upload process
-upload_images(local_images_folder, remote_images_folder)
+if __name__ == "__main__":
+    
+    # Run the script in the background using nohup and redirect output log to upload_log.log
+    os.system('nohup python3 upload.py > upload_log.log 2>&1 &')
+
+    try:
+        # Start monitoring the folder for changes
+        start_monitoring()
+    except KeyboardInterrupt:
+        # Handle KeyboardInterrupt to stop the script and terminate the backgrounf process
+        os.system("pkill -f 'python3 upload.py'")
 
 
+    
