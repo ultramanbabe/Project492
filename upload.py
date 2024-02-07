72e93b8faf4127daf642b645acbc9da29d1bc95e
Puriwong <ultraman_babeclub@hotmail.com>
2024-02-07 20:26:14 +0700

    adding check for deletions from local and delete from cloud

--- upload.py
+++ upload.py
@@ -12,6 +12,7 @@ firebase_admin.initialize_app(cred, {
     })
 bucket = storage.bucket()
 
+# Reading files as a list
 def list_files_in_local_folder(local_folder):
     file_list = []
     for root, dirs, files in os.walk(local_folder):
@@ -21,14 +22,13 @@ def list_files_in_local_folder(local_folder):
             file_list.append({'local_path': file_path, 'cloud_path': cloud_path})
     return file_list
 
+# Reading folder as a list
 def list_folders_in_local_folder(local_folder):
     for dir in dirs: 
         folder_path = os.path.join(root, dir)
         cloud_path = os.path.join('images', os.path.relpath(folder_path, local_folder))
         folder_list.append({'local_path': folder_path, 'cloud_path': cloud_path})
     return folder_list
-
-                            
 
 def list_files_in_firebase_storage():
     blobs = bucket.list_blobs()
@@ -40,10 +40,21 @@ def sync_files(local_files, cloud_files):
         if not cloud_modification_time or os.path.getmtime(local_file['local_path']) > cloud_modification_time:
             upload_file_to_firebase(local_file['local_path'], local_file['cloud_path'])
 
+    # Check for deletions
+    for cloud_path, cloud_modification_time in cloud_files.items():
+        local_path = os.path.join(local_folder, os.path.relpath(cloud_path, 'images'))
+        if not os.path.exists(local_path):
+            delete_file_from_firebase(cloud_path)
+
 def upload_file_to_firebase(local_path, cloud_path):
     blob = bucket.blob(cloud_path)
     blob.upload_from_filename(local_path)
     print(f'Uploaded {local_path} to Firebase under {cloud_path}.')
+
+def delete_file_from_firebase(local_path, cloud_path):
+    blob = bucket.blob(cloud_path)
+    blob.delete()
+    print(f'Deleted {local_path} from Firebase')
 
 
 
