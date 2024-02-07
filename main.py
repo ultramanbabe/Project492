6e4d1187af27975a5ba1da06317f74852d3bed8a
Puriwong <ultraman_babeclub@hotmail.com>
2024-02-07 20:52:14 +0700

    edited main.py for more clarification

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ images_folder = "/home/pi/project492/images"
 current_date = datetime.now().strftime("%d-%m-%Y")
 subfolder_name = current_date
 subfolder_path = os.path.join(images_folder, subfolder_name)
+current_time = datetime.now().strftime("%d-%m-%Y-%T")
 
 #check if the folder exists
 if not os.path.exists(subfolder_path):
@@ -18,7 +19,7 @@ if not os.path.exists(subfolder_path):
     os.mkdir(subfolder_path)
     print(f"Subfolder '{subfolder_name}' created succesfilly in '{images_folder}'.")
 else:
-    print(f"Subfolder '{subfolder_name}' already exist in '{images_folder}'.")
+    print(f"Subfolder '{subfolder_name}' already exist in '{images_folder}'. Saving the " + current_time +  " image.")
 
 #capture image
 subprocess.run('rpicam-jpeg --width 2592 --height 1944 -q 100 -t 3000 --shutter 200000 --gain 1.5 --verbose 0 -n -o /home/pi/project492/images/"$(date "+%d-%m-%Y")"/"$(date +%d-%m-%Y-%T)".jpg' , shell=True)
